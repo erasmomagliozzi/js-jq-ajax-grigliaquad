0915d522a069d7ef09158980bab3047c2a880727
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-01-30 15:38:25 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Griglia 6x6, ad ogni click sul quadrato parte una richiesta AJAX che prende un numero random da 1 a 9.
+// Se è <= 5 il quadrato diventa giallo,
+// se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato.
+// I quadrati fateli prima a mano e poi con javascript.
+$(document).ready(function(){
+
+  $('.quadrato').click(function(){
+     $(this).each(function(){
+       $.ajax(
+         {
+         url : "https://flynn.boolean.careers/exercises/api/random/int",
+         method : "GET",
+         success : function (data, stato){
+           $('.quadrato').html(data.response);
+           console.log(data);
+         },
+         error : function (richiesta, stato, errori) {
+           alert("E' avvenuto un errore. " + errore);
+           }
+       });
+       
+
+
+
+     });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
